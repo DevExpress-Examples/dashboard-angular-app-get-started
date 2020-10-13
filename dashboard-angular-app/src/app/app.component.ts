853078020f467f7218ff563cb38aa4213fb55e12
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2020-10-13 11:53:41 +0300

    Simplify implementation

--- dashboard-angular-app/src/app/app.component.ts
+++ dashboard-angular-app/src/app/app.component.ts
@@ -6,17 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dashboard-angular-app';
-  workingMode: string = 'Designer';
-  get workingModeText() {
-    return 'Switch to ' + this.toggleMode(this.workingMode);
-  }
   
-  changeWorkingMode() {    
-    this.workingMode = this.toggleMode(this.workingMode);
-  }
-  toggleMode(mode) {
-    return mode === 'Viewer' ? "Designer" : "Viewer";
-  }
-
 }
