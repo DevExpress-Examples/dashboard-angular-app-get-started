fa29ccde1dc6a442df4f70e5be58f0c1cdccebe1
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2022-01-27 20:26:21 +0300

    Add files via upload

---
+++ dashboard-angular-app/ngcc.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    packages: {
+      'devextreme-angular':  {
+        ignorableDeepImportMatchers: [
+          /devextreme\//
+        ]
+      },
+      'devexpress-dashboard-angular':  {
+        ignorableDeepImportMatchers: [
+          /devexpress-dashboard\//
+        ]
+      },
+    }
+  };
