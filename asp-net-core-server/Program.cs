1697f2960de6ef5bfee961acd0fce7c727390e45
Polina Tyureva <polina.tyureva@devexpress.com>
2022-11-16 19:59:21 +0400

    Update (BC 1127730)

--- asp-net-core-server/Program.cs
+++ asp-net-core-server/Program.cs
@@ -76,7 +76,7 @@ app.UseRouting();
 app.UseCors("CorsPolicy");
 
 // Maps the dashboard route.
-EndpointRouteBuilderExtension.MapDashboardRoute(app, "api/dashboard", "DefaultDashboard");
+app.MapDashboardRoute("api/dashboard", "DefaultDashboard");
 // Requires CORS policies.
 app.MapControllers().RequireCors("CorsPolicy");
 
