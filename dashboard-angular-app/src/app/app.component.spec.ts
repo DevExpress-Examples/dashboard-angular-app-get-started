9b65c531e48b95478a088aacc929d7c979aec127
Polina Tyureva <polina.tyureva@devexpress.com>
2023-12-26 13:37:17 +0400

    update to Angular v17

--- dashboard-angular-app/src/app/app.component.spec.ts
+++ dashboard-angular-app/src/app/app.component.spec.ts
@@ -1,14 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -16,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dashboard-angular-app'`, () => {
+  it(`should have the 'dashboard-angular-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('dashboard-angular-app');
@@ -25,7 +23,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dashboard-angular-app app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dashboard-angular-app');
   });
 });
--- dashboard-angular-app/src/app/app.component.ts
+++ dashboard-angular-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { DxDashboardControlModule } from 'devexpress-dashboard-angular';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+	CommonModule, 
+	RouterOutlet, 
+	DxDashboardControlModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  title = 'dashboard-angular-app';
 }
